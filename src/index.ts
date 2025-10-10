10e98f58ce5ef8472e6b16f0fdb96f4a62c6ab18
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-10-10 00:27:12 -0300

    feat(app): Add Swagger and Scalar Fastify API reference for improved documentation (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import fastify from 'fastify';
+import fastifySwagger from "@fastify/swagger";
+import fastify from "fastify";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import scalarFastify from "@scalar/fastify-api-reference";
 
 const server = fastify();
 
-server.get('/', async (request, reply) => {
-  return { hello: 'world' };
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Servicin API",
+      description: "API documentation for Servicin application",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(scalarFastify, {
+  routePrefix: "/docs",
+  configuration: {
+    metaData: {
+      title: "Servicin API",
+      description: "API documentation for Servicin application",
+    },
+  },
 });
 
 server.listen({ port: 8080 }, (err, address) => {
